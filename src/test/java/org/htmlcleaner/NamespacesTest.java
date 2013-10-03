613b253c14f195c2fda9a798f253e20ffe7ba9d8
scottwilson <scottwilson@482c6979-392e-0410-9d6c-a8d33d214f2b>
2013-10-03 09:12:06 +0000

    Added tests for bug #63
    git-svn-id: https://svn.code.sf.net/p/htmlcleaner/code/trunk@290 482c6979-392e-0410-9d6c-a8d33d214f2b

---
+++ src/test/java/org/htmlcleaner/NamespacesTest.java
@@ -0,0 +1,74 @@
+/*  Copyright (c) 2006-2013, the HtmlCleaner Project
+    All rights reserved.
+
+    Redistribution and use of this software in source and binary forms,
+    with or without modification, are permitted provided that the following
+    conditions are met:
+
+    * Redistributions of source code must retain the above
+      copyright notice, this list of conditions and the
+      following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the
+      following disclaimer in the documentation and/or other
+      materials provided with the distribution.
+
+    * The name of HtmlCleaner may not be used to endorse or promote
+      products derived from this software without specific prior
+      written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package org.htmlcleaner;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class NamespacesTest  extends AbstractHtmlCleanerTest{
+
+	/**
+	 * Uses an RDFa example to test that we retain namespace declarations. See issue #63
+	 * @throws IOException
+	 */
+    @Test
+    public void RDFa() throws IOException{
+		String initial = readFile("src/test/resources/test13.html");
+		String expected = readFile("src/test/resources/test13_expected.html");
+		assertCleaned(initial, expected);
+    }
+    
+    /**
+     * Uses a namespace prefix for an element. See issue #63
+     * @throws IOException
+     */
+    @Test
+    public void DCElement() throws IOException{
+		String initial = readFile("src/test/resources/test14.html");
+		String expected = readFile("src/test/resources/test14_expected.html");
+		assertCleaned(initial, expected);
+    }
+
+    /**
+     * Uses a namespace prefix for an attribute. See issue #63
+     * @throws IOException
+     */
+    @Test
+    public void DCAttribute() throws IOException{
+		String initial = readFile("src/test/resources/test15.html");
+		String expected = readFile("src/test/resources/test15_expected.html");
+		assertCleaned(initial, expected);
+    }
+}
